b67441bfe821857ef289f62a27bf80c4f6a86566
melaniecristeche <u943142818@byui.edu>
2023-11-22 01:13:56 +0100

    errors

--- server.js
+++ server.js
@@ -24,13 +24,32 @@ app.set("layout", "./layouts/layout") // not at views root/
 
 app.use(static)
 
+// File Not Found Route - must be last route in list
+app.use(async (req, res, next) => {
+  next({status: 404, message: 'Sorry, we appear to have lost that page.'})
+})
+
 // Index route
 
 app.get('/', baseController.buildHome) 
 // Inventory routes
 app.use("/inv", inventoryRoute)
 /* ***********************
- * Local Server Information
+
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message: err.message,
+    nav
+  })
+})
+ /* Local Server Information
  * Values from .env (environment) file
  *************************/
 const port = process.env.PORT
